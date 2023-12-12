7182d50c96f2a55f5dbd5d090bb4bae9ad488e55
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-12-11 21:58:35 -0300

    titulo e descricao

--- nowe-page/pages/components/Nav.jsx
+++ nowe-page/pages/components/Nav.jsx
@@ -6,13 +6,17 @@ export default function Nav(){
         <>
           <nav className={navstyle.navbar}>
             <div className={navstyle.navItems}>
-                <Link href="./home">Home</Link>
+                <Link href="./">Home</Link>
                 <Link href="./sobre">Sobre</Link>
                 <Link href="./contato">Contato</Link>
             </div>
             <div className={navstyle.navButton}>
                     <Link className={navstyle.weonBtn} href='https://nowecompany.com.br/weon/' target="_blank" rel="noopener noreferrer">weon</Link>
                 </div>
+
+
+
+                
         </nav>
         </>
     )
