6301cf970c8f0bd69bef451fba06cbd4cb566520
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2023-12-13 16:11:49 -0300

    pages

--- nowe-page/pages/index.jsx
+++ nowe-page/pages/index.jsx
@@ -1,34 +1,43 @@
 import index from "../styles/index.module.css"
 import Nav from '../pages/components/Nav'
+import Page from './components/home/Section2'
 import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
-<>
-  <Nav />
+    <>
+      <Nav />
+      <section id="intro">
+        <div className={index.backgroundContainer}>
+          <div className="container min-vh-100 d-flex align-items-center justify-content-center">
+            <div className="row w-100">
+              <div className="col-md-6">
+                <div className={index.contentlogo}>
+                  <img src="/Icon/white.png" className={index.logo} alt="logo-main" />
 
+                  <h1 className={index.subtitle}>
+                    <span className={index.destaque}>Weon</span> a revolução na eficiência dos seus dados.
+                  </h1>
 
-  <div className={index.backgroundContainer}>
+                  <div className={index.containerBtn}>
+                    <button className={index.buttonconhecamais}> Conhecer </button>
+                    <div className={index.icon}>
+                      <ion-icon name="arrow-forward-outline"></ion-icon>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-  <div className={`d-flex align-items-center justify-content-start min-vh-100`}>
-    <div>
-      <div className="position-relative">
-        <div className={index.contentlogo}>
-        <img src="/Icon/white.png" className={index.logo} alt="logo-main" />
-     
-      <h1 className={index.subtitle}>
-        <span className={index.destaque}>Weon</span> a revolução na eficiência dos seus dados.
-      </h1>
-      <button className={index.buttonconhecamais}> conhecer </button>
-    </div>
-      </div>
-    </div>
-  </div>
-  
-  
-  </div>
-</>
+              <div className="col-md-6">
+                <img src="/Img/cube.png" className={index.cube} alt="cube" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
+     <Page />
+    </>
   )
 }
