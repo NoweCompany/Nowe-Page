bea1bbe0514b294640571c7f1f154be54dfe49b5
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-02-09 21:50:09 -0300

    home alert

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,18 @@
 //componentes
-import Nav from '../pages/components/Nav'
-import Home from './components/home/Home'
-import Page from './components/home/Paginas'
-import WeonExplain from './components/home/WeonExplain'
-import Card from './components/home/Card'
-import Video from './components/home/Video'
-import Footer from './components/Footer'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import Nav from '../pages/components/Nav'
+// import Home from './components/home/Home'
+// import Page from './components/home/Paginas'
+// import WeonExplain from './components/home/WeonExplain'
+// import Card from './components/home/Card'
+// import Video from './components/home/Video'
+// import Footer from './components/Footer'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function app() {
   return (
-    <>
-    <Nav />
-    <Home /> 
+    <div className="body">
+    {/* <Nav /> */}
+    {/* <Home /> 
 
     <section id="scroll"></section>
     <Page />
@@ -21,7 +21,10 @@ export default function app() {
     <WeonExplain />
     <Card />
     <Video />
-    <Footer />
-    </>
+    <Footer /> */}
+    <div className='containerMain'> 
+    <h1 className="maintextAlert">Desculpa pelo transtorno, estamos em manutenção<spam className='bluedot'>.</spam> :(</h1>
+    </div>
+    </div>
   )
 }
